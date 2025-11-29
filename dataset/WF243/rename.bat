3de9ca8d26f003e12858d4e818b81f82b314c706
vickobgt-sketch <vicko.bgt@gmail.com>
2025-11-29 23:22:07 +0700

    Add files via upload

---
+++ dataset/WF243/rename.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set prefix=WF243
+set count=1
+
+for /f "delims=" %%f in ('dir /b /a-d /o:d *.jpg *.jpeg *.png') do (
+    ren "%%f" "!prefix!_!count!%%~xf"
+    set /a count+=1
+)
